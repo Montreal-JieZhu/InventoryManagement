67c8c01a165e3b9e9a15c1dcdc292526bacdb0e4
Wang, Jing <wjing.ca@gmail.com>
2018-06-24 16:54:14 -0400

    * commit by jing

---
+++ InventoryManagement/Controllers/MaterialController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagement.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View("MaterialForm");
+        }
+    }
+}
